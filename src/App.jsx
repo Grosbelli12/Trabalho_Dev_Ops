0c96d9ef5d3b9cb5cfa6dd46711a42c628603ccb
Grosbelli12 <gustavogrosbelli11@gmail.com>
2026-04-01 21:25:30 -0300

    style: estilizar títulos principais com tema dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 function App() {
-  return
+  return(
    <>
-  
-  </>;
+    <h1>Gerador de número aleatório</h1>
+    <h2>Selecione o intervalo que deseja gerar o número aleatório</h2>
+  </>
+  );
 }
 
 export default App;
