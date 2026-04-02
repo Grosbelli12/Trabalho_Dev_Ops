32026f53a1ce57456f7b12f09e445e4ee315ea75
Grosbelli12 <gustavogrosbelli11@gmail.com>
2026-04-01 21:32:09 -0300

    feat: adicionar inputs de intervalo com controle de estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return(
-   <>
-    <h1>Gerador de número aleatório</h1>
-    <h2>Selecione o intervalo que deseja gerar o número aleatório</h2>
-  </>
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(100);
+  return (
+    <>
+      <h1>Gerador de número aleatório</h1>
+      <h2>Selecione o intervalo que deseja gerar o número aleatório</h2>
+
+      <input
+        type="number"
+        placeholder="Digite o número minimo"
+        onChange={(e) => setNum1(Number(e.target.value))}
+      />
+      <input
+        type="number"
+        placeholder="Digite o número máximo"
+        onChange={(e) => setNum2(Number(e.target.value))}
+      />
+    </>
   );
 }
 
